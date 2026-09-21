c971c8bc956f9beb4509388228712a9f621130c0
GJCreatorSmpSeizoen1 <jaydenderooi0@gmail.com>
2026-09-21 22:03:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,15 @@ const clues = [
 let clueIndex = 0;
 
 function changeMessage() {
+
     const text = document.getElementById("message");
 
     text.style.opacity = "0";
 
     setTimeout(() => {
+
         text.textContent = clues[clueIndex];
+
         text.style.opacity = "1";
 
         clueIndex++;
@@ -22,11 +25,12 @@ function changeMessage() {
         if (clueIndex >= clues.length) {
             clueIndex = 0;
         }
+
     }, 200);
 }
 
-document.addEventListener("keydown", function(event) {
-    if (event.key === "x" || event.key === "X") {
-        document.body.classList.toggle("secret-mode");
-    }
-});
+function activateSecret() {
+
+    document.body.classList.toggle("secret-mode");
+
+}
